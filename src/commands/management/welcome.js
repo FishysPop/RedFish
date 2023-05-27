6464218c7bb843bfb4eab7319f9bea1546b5ac68
FishyPop <kyle.claassens@gmail.com>
2023-05-27 09:28:44 +0200

    made setup work

--- src/commands/management/welcome.js
+++ src/commands/management/welcome.js
@@ -10,17 +10,17 @@ module.exports = {
         .setDescription('The channel you want the welcome messages to be sent in.')
         .addChannelTypes(ChannelType.GuildText).setRequired(true))
         .addStringOption(option => option
-            .setName('type')
+            .setName('type') // 1 is only welcome | 2 is welcome and leaves | 3 is welcome and bans | 4 is welcome leaves and bans
             .setDescription('What messages will be send')
             .addChoices(
-				{ name: 'Welcome', value: 'welcome' },
-				{ name: 'Welcome and Leave', value: 'welcomeLeave' },
-                { name: 'Welcome and Ban', value: 'welcomeBan' },
-				{ name: 'Welcome, Leave and Ban', value: 'welcomeLeaveBan' },
+				{ name: 'Welcome', value: '1' },
+				{ name: 'Welcome and Leave', value: '2' },
+                { name: 'Welcome and Ban', value: '3' },
+				{ name: 'Welcome, Leave and Ban', value: '4' },
 			).setRequired(true))
                    .addStringOption(option => option
                    .setName('welcome-message')
-                   .setDescription('The message that is send when a user joins.').setRequired(true))
+                   .setDescription('The message that is send when a user joins | (@user) to mention a user | (server) servers name'))
                       .addStringOption(option => option
                       .setName('leave-message')
                       .setDescription('The message that is send when a user leaves.'))
@@ -41,7 +41,25 @@ module.exports = {
      const subcommand = interaction.options.getSubcommand();
      const welcome = await Welcome.findOne({ guildId: interaction.guild.id });
      if (subcommand === 'setup' ) {
- 
+        const channel = interaction.options.getChannel('channel').id
+        const type = interaction.options.getString('type')
+        const welcomeMessage = interaction.options.getString('welcome-message')?.value || `Welcome (user) to (server)!`;
+        const leaveMessage = interaction.options.getString('leave-message')?.value || '(user) has left (server)!';
+        const banMessage = interaction.options.getString('ban-message')?.value || '(user) has been banned from (server)!';
+        if (welcome) {
+            await interaction.editReply(`Ticket system has already been setup tickets will be created in ${Ticket.Category} to disable run **/ticket disable**`)
+            return;
+       } else {
+        Welcome.create({
+         guildId: interaction.guild.id,
+         channel: channel,
+         type: type,
+         welcomeMessage: welcomeMessage,  
+         banMessage: banMessage,     
+         leaveMessage: leaveMessage,
+})
+         interaction.editReply("Welcome has been setup.")
+       }
      }
      if (subcommand === 'quick-setup' ) {
  
--- src/models/Welcome.js
+++ src/models/Welcome.js
@@ -6,6 +6,10 @@ const WelcomeSchema = new Schema({
     required: true,
     unique: true,
   },
+  channel: {
+    type: String,
+    required: true,
+  },
   type: {
     type: Number,  // 1 is only welcome | 2 is welcome and leaves | 3 is welcome and bans | 4 is welcome leaves and bans
     required: true,
@@ -13,20 +17,14 @@ const WelcomeSchema = new Schema({
   },
   welcomeMessage: {
     type: String,
-    default: "has joined.",
     required: true,
   },
   banMessage: {
     type: String,
-    default: "has been banned.",
-    required: true,
   },
   leaveMessage: {
     type: String,
-    default: "has left the server.",
-    required: true,
   },
-
 });
 
 module.exports = model('Welcome', WelcomeSchema);

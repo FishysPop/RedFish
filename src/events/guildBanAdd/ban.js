376c6aef5f52f0987762b1e472b8a93fe9ce2ba2
FishyPop <kyle.claassens@gmail.com>
2023-09-12 14:00:01 +0200

    more fixes

--- src/events/guildBanAdd/ban.js
+++ src/events/guildBanAdd/ban.js
@@ -2,13 +2,12 @@ const { Client, GuildMember } = require('discord.js');
 const Welcome = require('../../models/Welcome');
 
 module.exports = async (ban, client ,handler) => {
-  try {
-    let guild = ban.guild;
-    if (!guild) return;
+  const welcome = await Welcome.findOne({ guildId: guild.id });
+  const guild = ban.guild;
 
-    const welcome = await Welcome.findOne({ guildId: guild.id });
-    console.log(welcome.type)
-    if (!welcome) return;
+  if (!welcome) return;
+  if (!guild) return;
+  try {
     if (welcome.type === 2) return;
     if (welcome.type === 1) return;
     const channel = client.channels.cache.get(welcome.channel);
@@ -16,6 +15,7 @@ module.exports = async (ban, client ,handler) => {
     const message = userMessage.replace('(server)',`${ban.guild}`);
     channel.send(`${message}`).catch((err) => {client.users.send(guild.ownerId, `I do not have permissions to send messages in ${channel}`);});
 } catch (error) {
-    console.log(`Error while sending welcome: ${error}`);
+   client.users.send(guild.ownerId, 'Welcome channel has been deleted please disable welcome or set it up again');
+    console.log(`Error while sending ban message: ${error}`);
   }
 };

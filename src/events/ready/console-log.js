dfe78a4b8888a58a5884a88edad1675d4bba2aa3
FishyPop <kyle.claassens@gmail.com>
2023-11-08 07:56:08 +0200

    fixed not working on start

--- src/events/ready/console-log.js
+++ src/events/ready/console-log.js
@@ -1,6 +1,7 @@
 const { ActivityType } = require('discord.js');
 module.exports = (client) => {
     console.log(`${client.user.tag} is online`);
+    client.user.setActivity('/play', { type: ActivityType.Watching });
     try {
         var timerID = setInterval(async function() {
         client.user.setActivity('/play', { type: ActivityType.Watching });

9d69eec6acdf2801c60943cdf475c2d1d34d89fd
FishyPop <kyle.claassens@gmail.com>
2023-11-08 07:52:19 +0200

    fixed status going away

--- src/events/ready/console-log.js
+++ src/events/ready/console-log.js
@@ -1,6 +1,14 @@
 const { ActivityType } = require('discord.js');
 module.exports = (client) => {
     console.log(`${client.user.tag} is online`);
-    client.user.setActivity('/play', { type: ActivityType.Watching });
+    try {
+        var timerID = setInterval(async function() {
+        client.user.setActivity('/play', { type: ActivityType.Watching });
+
+        }, 60 * 60 * 1000)
+    } catch (error) {
+        console.log(error)
+
+    }
 
 };

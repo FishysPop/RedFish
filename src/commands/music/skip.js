058ea163b16be3b05187fe945eab743e10866551
FishyPop <kyle.claassens@gmail.com>
2024-02-18 03:14:32 +0200

    added lavalink support to skip

--- src/commands/music/skip.js
+++ src/commands/music/skip.js
@@ -5,26 +5,51 @@ module.exports =  {
     .setName("skip")
     .setDescription("Skips a song."),
 
-  run: ({ interaction, client, handler }) => {
+  run: async ({ interaction, client, handler }) => {
     if (!interaction.inGuild()) {
-      interaction.reply({
-        content: "You can only run this command in a server.",
-        ephemeral: true,
-      });
+      interaction.reply({ content: "You can only run this command in a server.", ephemeral: true,});
      return;
     }
-   const queue = useQueue(interaction.guildId)
-   if (!interaction.member.voice.channel) {
-    interaction.reply({content: 'You are not connected to a voice channel.',ephemeral: true})
-    return;
-}
-if (!queue || !queue.isPlaying()) {
-    interaction.reply({content: `There is nothing currently playing. \nPlay something using **\`/play\`**`,ephemeral: true})
-    return;
-}
-    queue.node.skip()
-    interaction.reply("Track Skipped")
-   
+    if (!interaction.member.voice.channel) {
+      interaction.reply({content: 'You are not connected to a voice channel.',ephemeral: true})
+      return;
+  }
+  switch (client.playerType) {
+    case "both":
+      const Lavaplayer = client.manager.players.get(interaction.guild.id);
+      const Discordplayer = useQueue(interaction.guild.id)
+      if (!Lavaplayer && !Discordplayer) {
+       return interaction.reply({content: `There is nothing currently playing. \nPlay something using **\`/play\`**`,ephemeral: true})
+      }
+      if (Discordplayer) {
+       Discordplayer.node.skip()
+       interaction.reply("Track Skipped")
+      } else if (Lavaplayer) {
+       Lavaplayer.skip();
+        interaction.reply("Track Skipped")
+      } else {
+        return interaction.reply({content: `There is nothing currently playing. \nPlay something using **\`/play\`**`,ephemeral: true})
+      }
+    break;
+    case "lavalink":
+      const player = client.manager.players.get(interaction.guild.id);
+      if (!player) {
+        return interaction.reply({content: `There is nothing currently playing. \nPlay something using **\`/play\`**`,ephemeral: true})
+       }
+        player.skip();
+        interaction.reply("Track Skipped")
+    break;
+    case "discord_player":
+      const queue = useQueue(interaction.guildId)
+      if (!queue || !queue.isPlaying()) {
+        interaction.reply({content: `There is nothing currently playing. \nPlay something using **\`/play\`**`,ephemeral: true})
+        return;
+    }
+        queue.node.skip()
+        interaction.reply("Track Skipped")
+    break;
+  }
+
   },
 
   // devOnly: Boolean,

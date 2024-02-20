d97c3ac1ead2f886c36ebfcee08fa6e51eabfad6
FishyPop <kyle.claassens@gmail.com>
2024-02-20 22:14:18 +0200

    updated error handling

--- src/events/errors/handleErrors.js
+++ src/events/errors/handleErrors.js
@@ -1,10 +1,27 @@
 const process = require("node:process");
+const { DiscordAPIError } = require('discord.js');
 require("dotenv").config();
 module.exports = (client) => {
 
-process.on('unhandledRejection', async (reason, promise) => {
-    console.log("unhandled rejection at:", promise, 'reason:', reason)
-});
+    process.on('unhandledRejection', async (reason, promise) => {
+        console.log("unhandled rejection at:", promise, 'reason:', reason);
+        try {
+        if (reason instanceof DiscordAPIError && reason.code === 50001) {
+            console.log("Error occurred due to Missing Access.");
+            
+            // Extracting relevant information for the request
+            const requestBody = reason.requestBody;
+            const embeds = requestBody?.json?.embeds;
+            const content = requestBody?.json?.content;
+    
+            console.log("Embeds:", embeds);
+            console.log("Content:", content);
+        }
+    } catch (error) {
+    return;            
+    }
+    });
+    
 
 process.on('uncaughtException', (err) => {
     console.log("Uncaught Exception:", err);

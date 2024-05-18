cd50f55496f0bf126c42b2e38d5ba3f4d1fff3a1
FishyPop <kyle.claassens@gmail.com>
2024-05-18 23:09:50 +0200

    half migrated radio to lavalink and autorole disable if missing perms

--- src/commands/music/radio.js
+++ src/commands/music/radio.js
@@ -1,5 +1,5 @@
 const { Client, Interaction, ApplicationCommandOptionType , SlashCommandBuilder, EmbedBuilder ,ComponentType ,PermissionsBitField} = require("discord.js");
-const { Player, QueryType } = require('discord-player');
+const { Player, QueryType, useMainPlayer } = require('discord-player');
 const axios = require('axios')
 module.exports =  {
     data: new SlashCommandBuilder()
@@ -12,7 +12,6 @@ module.exports =  {
 
 
   run: async({ interaction, client, handler }) => {
-    if (client.playerType === 'lavalink') return interaction.reply({content: 'Lavalink is not supported yet',ephemeral: true,});
     if (!interaction.inGuild()) {
       interaction.reply({
         content: "You can only run this command in a server.",
@@ -32,49 +31,113 @@ module.exports =  {
       if (data.length < 1) {
         return await interaction.followUp({ content: `❌ | No radio station was found, A full list can be found [here](https://www.radio-browser.info/search?page=1&hidebroken=true&order=votes&reverse=true)` })}
 
-      const searchResult = await player.search(data[0].url_resolved, {
-        requestedBy: interaction.user,
-      });
+    switch (client.playerType) {
+      case "both":
+        break;
 
-        const res = await player.play(
-            interaction.member.voice.channel.id,
-            searchResult, 
-            {
-              nodeOptions: {
-                metadata: {
-                  channel: interaction.channel,
-                  client: interaction.guild.members.me,
-                  requestedBy: interaction.user,
-
-
+        case "discord_player":
+            const player = useMainPlayer();
+            const searchResult = await player.search(data[0].url_resolved, {
+              requestedBy: interaction.user,
+            });
+            const res = await player.play(
+              interaction.member.voice.channel.id,
+              searchResult, 
+              {
+                nodeOptions: {
+                  metadata: {
+                    channel: interaction.channel,
+                    client: interaction.guild.members.me,
+                    requestedBy: interaction.user,
+  
+  
+                  },
+                  bufferingTimeout: 15000,
+                  leaveOnEmpty: true,
+                  leaveOnEmptyCooldown: 300000,
+                  skipOnNoStream: true,
+                  connectionTimeout: 999_999_999
                 },
-                bufferingTimeout: 15000,
-                leaveOnEmpty: true,
-                leaveOnEmptyCooldown: 300000,
-                skipOnNoStream: true,
-                connectionTimeout: 999_999_999
-              },
-            }
-          );
- 
-          if (!interaction.guild.members.me.permissionsIn(interaction.channel).has(PermissionsBitField.Flags.ViewChannel) || !interaction.guild.members.me.permissionsIn(interaction.channel).has(PermissionsBitField.Flags.SendMessages)) {
+              }
+            );
+   
+            if (!interaction.guild.members.me.permissionsIn(interaction.channel).has(PermissionsBitField.Flags.ViewChannel) || !interaction.guild.members.me.permissionsIn(interaction.channel).has(PermissionsBitField.Flags.SendMessages)) {
+                  const embed = new EmbedBuilder()
+                      .setColor('#e66229')
+                      .setDescription(`**Enqueued: [${name}](${res.track.url}) -** \`LIVE\``)
+                      .setFooter({ text: `Media Controls Disabled: Missing Permissions` });
+                  return interaction.editReply({ embeds: [embed] });
+          } else {
                 const embed = new EmbedBuilder()
-                    .setColor('#e66229')
-                    .setDescription(`**Enqueued: [${name}](${res.track.url}) -** \`LIVE\``)
-                    .setFooter({ text: `Media Controls Disabled: Missing Permissions` });
-                return interaction.editReply({ embeds: [embed] });
-        } else {
-              const embed = new EmbedBuilder()
+                .setColor('#e66229')
+                .setDescription(`**Enqueued: [${name}](${res.track.url}) -** \`LIVE\``)
+                 return interaction.editReply({ embeds: [embed] });
+            }
+         
+ 
+        break;
+
+        case "lavalink":
+          try {
+          const player = await client.manager.createPlayer({
+            guildId: interaction.guild.id,
+            textId: interaction.channel.id,
+            voiceId: channel.id,
+            volume: 30,
+            deaf: true
+        });
+
+        const res = await player.search(data[0].url_resolved, { requester: interaction.user });
+        if (!res.tracks.length) return interaction.editReply("No results found!");
+        player.queue.add(res.tracks[0]);
+        if (!player.playing && !player.paused) player.play();
+
+        if (!interaction.guild.members.me.permissionsIn(interaction.channel).has(PermissionsBitField.Flags.ViewChannel) || !interaction.guild.members.me.permissionsIn(interaction.channel).has(PermissionsBitField.Flags.SendMessages)) {
+          const embed = new EmbedBuilder()
               .setColor('#e66229')
-              .setDescription(`**Enqueued: [${name}](${res.track.url}) -** \`LIVE\``)
-               return interaction.editReply({ embeds: [embed] });
-          }
-        } 
-          catch (e) {
-        // let's return error if something failed
-        console.log(`Error with radio `, 'query: ', name ,'error: ', e)
-        return interaction.editReply(`Unable to play ${name} due to an error`);
+              .setDescription(`**Enqueued: [${name}](${res.tracks[0].uri}) -** \`LIVE\``)
+              .setFooter({ text: `Media Controls Disabled: Missing Permissions` });
+            return interaction.editReply({ embeds: [embed] });
+       } else {
+             const embed = new EmbedBuilder()
+             .setColor('#e66229')
+             .setDescription(`**Enqueued: [${name}](${res.tracks[0].uri}) -** \`LIVE\``)
+            return interaction.editReply({ embeds: [embed] });
     }
+        if (res.type === "PLAYLIST") {
+            for (let track of res.tracks) player.queue.add(track);
+
+            if (!player.playing && !player.paused) player.play();
+
+            const embed = new EmbedBuilder()
+                .setColor('#e66229')
+                .setDescription(`**Enqueued: [${res.playlistName}](${name}) (${res.tracks.length} tracks**)`)
+            return interaction.editReply({ content: " ", embeds: [embed] })
+        } else {
+            player.queue.add(res.tracks[0]);
+
+            if (!player.playing && !player.paused) player.play();
+            const embed = new EmbedBuilder()
+                .setColor('#e66229')
+                .setDescription(`**Enqueued: [${res.tracks[0].title}](${res.tracks[0].uri}) - ${res.tracks[0].author}** \`${convertTime(res.tracks[0].length, true)}\``)
+            return interaction.editReply({ content: " ", embeds: [embed] })
+        }
+      }
+        catch (e) {
+          console.log(e)
+      return interaction.editReply(`Something went wrong: ${e}`);
+        }
+        break;
+    
+      default:
+        break;
+    }
+  } 
+  catch (e) {
+    console.log(`Error with radio `, 'query: ', name ,'error: ', e)
+    return interaction.editReply(`Unable to play ${name} due to an error`);
+}
+
   }
 
   // devOnly: Boolean,
--- src/events/guildMemberAdd/autoRole.js
+++ src/events/guildMemberAdd/autoRole.js
@@ -1,16 +1,45 @@
-const { Client, GuildMember } = require('discord.js');
+const { Client, GuildMember, PermissionsBitField } = require('discord.js');
 const AutoRole = require('../../models/AutoRole');
 
-module.exports = async (member, client ,handler) => {
-  try {
+module.exports = async (member, client, handler) => {
+
     let guild = member.guild;
     if (!guild) return;
 
     const autoRole = await AutoRole.findOne({ guildId: guild.id });
     if (!autoRole) return;
 
+    if (!guild.members.me.permissions.has(PermissionsBitField.Flags.ManageRoles)) {
+      await AutoRole.deleteOne({ guildId: guild.id });
+
+      const owner = guild.ownerId;
+      try {
+      if (owner) {
+        client.users.send(owner, `AutoRole has been disabled for your guild "${guild.name}" because the bot does not have the required permissions to manage roles.`);
+      }
+      return;
+    } catch (error) {
+     return;   
+    }
+    }
+
+    const role = guild.roles.cache.get(autoRole.roleId);
+    if (!role || role.position >= guild.members.me.roles.highest.position) {
+
+      await AutoRole.deleteOne({ guildId: guild.id });
+
+      const owner = guild.ownerId;
+      
+      try {
+      if (owner) {
+        client.users.send(owner,`AutoRole has been disabled for your guild "${guild.name}" because the role "${role.name}" is higher than the bot's highest role.`);
+      }
+      return;
+    } catch (error) {
+      return;
+    }
+    }
+
     await member.roles.add(autoRole.roleId);
-  } catch (error) {
-    console.log(`Error giving role automatically: ${error}, Guild: (${member.guild.name})[${member.guild.id}]`);
-  }
+
 };

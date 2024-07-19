83b1ec2045e42d3764bed878cd6eed4fe5188801
FishyPop <kyle.claassens@gmail.com>
2024-07-19 22:23:40 +0200

    fix again

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -250,7 +250,7 @@ case "discord_player":
         const resultsSpotifyLavalink = await client.manager.search(query, { engine: 'spotify'}).catch(e => null) || null;
         const tracksSoundcloudLavalink = resultsSoundcloudLavalink.tracks[0] ? resultsSoundcloudLavalink.tracks.slice(0, 5).map((t) => ({
           name: `SoundCloud: ${`${t.title} - ${t.author} (${convertTime(t.length, true)})`.length > 70 ? `${`${t.title} - ${t.author}`.substring(0, 70)}... (${convertTime(t.length, true)})` : `${t.title} - ${t.author} (${convertTime(t.length, true)})`}`,
-          value: t.uri.length > 99 ? `${`${t.title} ${t.author} `.substring(0, 99)}}` : t.uri,
+          value: t.uri.length > 95 ? `${`${t.title} ${t.author} `.substring(0, 95)}}` : t.uri,
       })): [{ name: "No SoundCloud Results Found", value: query}];  
   
       const tracksSpotifyLavalink = resultsSpotifyLavalink ? resultsSpotifyLavalink.tracks.slice(0, 5).map((t) => ({

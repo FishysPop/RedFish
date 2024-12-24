c2ec61f3cab4d7b219026615675c93405f282cec
FishyPop <kyle.claassens@gmail.com>
2024-12-24 18:39:09 +0200

    fixed again

--- src/events/giveawayEvents/checkGiveaway.js
+++ src/events/giveawayEvents/checkGiveaway.js
@@ -55,10 +55,7 @@ var timerID = setInterval(async function() {
     const row = new ActionRowBuilder().addComponents(giveawayEnterButton);
     if (guild) {
       const channel = guild.channels.cache.get(firstGiveaway.channelId);
-      if (!channel) {
-        console.error("Channel not found for giveaway:", giveaway.channelId);
-          await Giveaway.deleteOne({ _id: giveaway._id });
-      }
+      if (!channel) return await Giveaway.deleteOne({ _id: giveaway._id });
       try {
         const message = await channel.messages.fetch(firstGiveaway.messageId);
         message.edit({
@@ -71,7 +68,6 @@ var timerID = setInterval(async function() {
         
       } catch (error) {
         console.error("Error editing giveaway message or finding it:", error);
-        await Giveaway.deleteOne({ _id: giveaway._id });  
 
       }
     }
